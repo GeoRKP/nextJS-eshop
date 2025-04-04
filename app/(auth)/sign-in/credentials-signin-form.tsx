3e83b54da33a98c59c3a9c9f18303cbd8e0db267
gkapatsinas <gkapatsinas@cognity.gr>
2025-04-04 21:52:40 +0300

    Enhance sign-in form: integrate action state management and loading indicator for sign-in button. Display error messages on failed sign-in attempts.

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -5,10 +5,28 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { Loader2 } from "lucide-react";
+
 
 export default function CredentialsSignInForm() {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button className="w-full" variant="default" type="submit" disabled={pending}>
+        {pending ? <Loader2 className="w-4 h-4 mr-2 animate-spin" /> : "Sign In"}
+      </Button>
+    );
+  };
   return (
-    <form>
+    <form action={action}>
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
@@ -33,10 +51,13 @@ export default function CredentialsSignInForm() {
           />
         </div>
         <div>
-          <Button className="w-full" variant="default" type="submit">
-            Sign In
-          </Button>
-          <div className="text-sm text-center text-muted-foreground mt-4">
+          <SignInButton />
+          {data && !data.success && (
+            <div className=" text-center text-destructive">
+              {data.message}
+            </div>
+          )}
+          <div className="text-sm text-center text-muted-foreground mt-2">
             Don&apos;t have an account? <Link href="/sign-up">Sign Up</Link>
           </div>
         </div>
